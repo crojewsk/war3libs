1db40745742ea72a4082f9df80c115b2ae282e7a
Cezary Rojewski <b4nnar@gmail.com>
2018-04-28 18:49:53 +0200

    Uploaded the actual run.ps1 script.

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,9 +1,9 @@
 ﻿<#
- Copyright (c) 2018 ITT Development team
-    Author: Cezary Rojewski
+ Copyright (c) 2018 Cezary Rojewski
 
 .DESCRIPTION
-    Given the input params, attempts to compile map script using jasshelper.
+    Given the input params, attempts to build map using jasshelper
+    and if successful, run it.
 
 .REMARKS
     Although no parameters are specified, any provided will be added to
@@ -12,21 +12,27 @@
     Checkout jasshelper manual for more infomation about available arguments.
 
 .EXAMPLE
-    compile.ps1 --debug  # '--debug' option will be added to command line
+    run.ps1 --debug  # '--debug' option will be added to command line
 #>
 
 $ScriptPath  = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
+
+$Warcraft3   = 'E:\Games\Warcraft III\Warcraft III.exe'
 $CommonJ     = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\common.j'
 $BlizzardJ   = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\blizzard.j'
-$Jasshelper  = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\jasshelper.exe'
 
+# For run purposes, clijasshelper is prefered due to stdout redirection
+$Jasshelper  = Join-Path -Path $ScriptPath -ChildPath 'bin\jasshelper\clijasshelper.exe'
 # Path to directory containing all .j and .zn files to be compiled using jasshelper
 $JassFolder  = Join-Path -Path $ScriptPath -ChildPath 'jass'
 # Path to unparsed .j file generated prior to compilation
 $GeneratedJ  = Join-Path -Path $ScriptPath -ChildPath 'bin\generated.j'
-# Path to output .j file to be created after compilation
-$OutputJ     = Join-Path -Path $ScriptPath -ChildPath 'wurst\war3map.j'
+# Path to base map
+$MapW3X      = Join-Path -Path $ScriptPath -ChildPath 'build1.w3x'
+# Path to output map w3x file to be created after successful build
+$OutputW3X   = Join-Path -Path $ScriptPAth -ChildPath 'bin\ITT2.w3x'
+
 
 # Create GeneratedJ file from content of all .j and .zn files
 $InputFiles  = Get-ChildItem -Path $JassFolder -Include *.j, *.zn -Recurse
@@ -46,10 +52,19 @@ ForEach ($File in $InputFiles)
     }
 }
 
-# Run jasshelper to perform actual compilation of map script
-$Params = $args + @('--scriptonly', $CommonJ, $BlizzardJ, $GeneratedJ, $OutputJ)
-#$ret = & $Jasshelper $Params # | Wait-Process
-#Start-Process -FilePath $Jasshelper -ArgumentList $Params -Wait
-Write-Host 'output: ' $ret
+Copy-Item $MapW3X $OutputW3X -Force
 
-& 'E:\Games\Warcraft III\Warcraft III.exe' @('-window', '-loadfile', 'C:\Users\Bannar\Documents\GitHub\island-troll-tribes\_build\Island Troll Tribes v3.0k.w3x')
+# Run jasshelper to perform actual compilation of map script
+$Params = $args + @($CommonJ, $BlizzardJ, $GeneratedJ, $OutputW3X)
+$Ret = & $Jasshelper $Params
+
+If ($Ret -Match 'Success!$')
+{
+    # Run the map
+    & $Warcraft3 @('-window', '-loadfile', $OutputW3X)
+}
+Else
+{
+    Write-Host $Ret
+}
+
