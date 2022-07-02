e4f5305b4f8777599dea4e295c3993fe6f7220f9
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2022-07-02 16:53:07 +0200

    Account for dependencies when compiling
    Update build script so that all dependencies are added to the source
    file list before compilation starts. For project to compile, ensure that
    the map initialization functions found in 'war3map.j' are located at the
    bottom of the generated file.
    
    As script is intended for compilation of jass files found within 'src'
    directory, drop JassSrc variable and declare new members to denote
    project's specifics.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,22 +10,19 @@
 .PARAMETER JassHelper
     # Full path specifying location of jasshelper.exe compiler.
 
-.PARAMETER JassSrc
-    # Root directory containing Jass and Zinc files to compile.
-
 .PARAMETER Output
     # Map file to inject compiled code into or name of an output file to create.
     # If Output does not end with '.w3x', then the jasshelper.exe is run with
     # --scriptonly option.
 
 .EXAMPLE
-    build.ps1 include/ jasshelper.exe src/ output.w3x
+    build.ps1 include/ jasshelper.exe output.w3x
 #>
-Param ($JassInclude, $JassHelper, $JassSrc, $Output)
+Param ($JassInclude, $JassHelper, $Output)
 
-$Usage = 'Usage: build.ps1 JASS_INCLUDE JASS_HELPER JASS_SRC OUTPUT'
+$Usage = 'Usage: build.ps1 JASS_INCLUDE JASS_HELPER OUTPUT'
 
-If (!$JassInclude -or !$JassHelper -or !$JassSrc -or !$Output)
+If (!$JassInclude -or !$JassHelper -or !$Output)
 {
     Write-Host $Usage
     return
@@ -34,10 +31,19 @@ If (!$JassInclude -or !$JassHelper -or !$JassSrc -or !$Output)
 $CommonJ = Join-Path -Path $JassInclude -ChildPath 'common.j'
 $BlizzardJ = Join-Path -Path $JassInclude -ChildPath 'blizzard.j'
 $TemporaryJ = ".tmp.j"
+$MainJ = "war3map.j"
+$BuildDir = "build"
+$IncludeDir = "..\include"
+$SrcDir = "..\src"
 
 try
 {
-    $AllSrcFiles = Get-ChildItem -Path $JassSrc -Include *.j, *.zn -Recurse -ErrorAction Stop
+    Set-Location -Path $BuildDir -ErrorAction Stop
+
+    # Map initialization functions are to be placed at the bottom
+    $AllSrcFiles = Get-ChildItem -Path $IncludeDir -Include *.j, *.zn -Exclude $MainJ -Recurse -ErrorAction Stop
+    $AllSrcFiles += Get-ChildItem -Path $SrcDir -Include *.j, *.zn -Recurse -ErrorAction Stop
+    $AllSrcFiles += Join-Path -Path $IncludeDir -ChildPath $MainJ
     New-Item $TemporaryJ -ItemType File -Force -ErrorAction Stop
 }
 catch [System.Exception]
